0dc58d86d36721a0d774e056ef7bdba110301772
Iván Hernández <73967631+ivanhernandez-dev@users.noreply.github.com>
2024-04-14 10:35:54 +0200

    Añadir primer test

--- apps/main/tv/codely/apps/inventory/backend/InventoryBackendApplication.java
+++ apps/main/tv/codely/apps/inventory/backend/InventoryBackendApplication.java
@@ -8,7 +8,9 @@ import tv.codely.shared.domain.Service;
 
 import java.util.HashMap;
 
+// DT Initialize the Spring Boot application
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+// DT Enable autowiring for the classes annotated with @Service in the following packages
 @ComponentScan(
 	includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
 	value = { "tv.codely.shared", "tv.codely.inventory", "tv.codely.apps.inventory.backend" }
