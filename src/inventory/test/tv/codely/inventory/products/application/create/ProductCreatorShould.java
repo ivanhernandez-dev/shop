e07ae34dee91220d45e8126379e1fc11b8ef44b3
Iván Hernández <73967631+ivanhernandez-dev@users.noreply.github.com>
2024-04-14 15:22:17 +0200

    Refactorizar y añadir tests

--- src/inventory/test/tv/codely/inventory/products/application/create/ProductCreatorShould.java
+++ src/inventory/test/tv/codely/inventory/products/application/create/ProductCreatorShould.java
@@ -1,24 +1,33 @@
 package tv.codely.inventory.products.application.create;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tv.codely.inventory.products.ProductsModuleUnitTestCase;
 import tv.codely.inventory.products.domain.*;
 
 import java.math.BigDecimal;
 
 import static org.mockito.Mockito.*;
 
-final class ProductCreatorShould {
-	@Test
-	void save_a_valid_product() throws Exception {
-		ProductRepository repository = mock(ProductRepository.class);
-		ProductCreator creator = new ProductCreator(repository);
+final class ProductCreatorShould extends ProductsModuleUnitTestCase {
+	private ProductCreator creator;
 
+	@BeforeEach
+	protected void setUp() {
+		super.setUp();
+
+		this.creator = new ProductCreator(repository);
+	}
+
+
+	@Test
+	void save_a_valid_product() {
 		CreateProductRequest request = CreateProductRequestMother.random();
 
 		Product product = ProductMother.fromRequest(request);
 
-		creator.create(request);
+		this.creator.create(request);
 
-		verify(repository, atLeastOnce()).save(product);
+		shouldHaveSaved(product);
 	}
 }
